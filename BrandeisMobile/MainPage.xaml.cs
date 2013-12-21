263334844cd13c6b7aa15e3dacc5f18621bf551f
Jessica Huynh <jhuynh37@brandeis.edu>
2013-12-20 20:15:29 -0600

    Basic navigation, started ApplicationBar, About and Athletic pages

---
+++ BrandeisMobile/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace BrandeisMobile
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Color DeisBlueColor = Color.FromArgb(255, 0, 112, 255);
+
+            ApplicationBar = ThemeManager.CreateApplicationBar();
+            ApplicationBar.Mode = ApplicationBarMode.Minimized;
+            ApplicationBar.Opacity = .8;
+            ApplicationBar.IsVisible = true;
+            ApplicationBar.IsMenuEnabled = true;
+            ApplicationBar.BackgroundColor = DeisBlueColor;
+            ApplicationBar.ForegroundColor = Color.FromArgb(255, 255, 255, 255); //white
+     
+            ApplicationBarIconButton linksIcon = new ApplicationBarIconButton();
+            linksIcon.IconUri = new Uri("/Images/favs.png", UriKind.Relative);
+            linksIcon.Text = "links";
+            ApplicationBar.Buttons.Add(linksIcon);
+            
+            ApplicationBarMenuItem full_websiteMenuItem = new ApplicationBarMenuItem();
+            full_websiteMenuItem.Text = "full website";
+            ApplicationBar.MenuItems.Add(full_websiteMenuItem);
+            full_websiteMenuItem.Click += new EventHandler(full_websiteMenuItem_Click);
+
+            ApplicationBarMenuItem aboutMenuItem = new ApplicationBarMenuItem();
+            aboutMenuItem.Text = "about";
+            ApplicationBar.MenuItems.Add(aboutMenuItem);
+            aboutMenuItem.Click += new EventHandler(aboutMenuItem_Click);
+
+            // Set the data context of the listbox control to the sample data
+            DataContext = App.ViewModel;
+        }
+
+        // Load data for the ViewModel Items
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+
+        private void full_websiteMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void aboutMenuItem_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/About.xaml", UriKind.Relative));
+        }
+
+        private void DirectorySearch_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            DirectorySearch.Text = "";
+        }
+
+    }
+}
